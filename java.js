311b05bccdbcdb28143fb6b906d81026540088d3
CaioFelps007 <125682381+CaioFelps007@users.noreply.github.com>
2023-04-19 13:24:02 -0300

    commit do java 2.0

---
+++ java.js
@@ -0,0 +1,15 @@
+function cubo(a, b) {
+    return Math.pow(a, 3)
+}
+
+
+
+function temperatura(fahrenheit) {
+let celsius = (fahrenheit -32) * 5 /9;
+return celsius;
+}
+
+
+function areaTriangulo(base, altura) {
+return (base * altura) / 2;
+}
